7bb39b1c4006d7032ac3e174c05358109d5cc71a
dogpantz <dogpantz@gmail.com>
2013-11-19 16:27:18 -0700

    Changed fader to instanced fader

--- src/utils.js
+++ src/utils.js
@@ -50,7 +50,7 @@ function storyFadeOut(callback) {
     if (!_.isFunction(callback))
         fail('storyFadeOut: callback is not a function');
 
-    Game.fader.fade(Game.fader.fadeTime, 'out', callback, true);
+    Crafty.e('Fader').fade('out', callback, true);
 }
 
 function showGirlLesson(e) {
