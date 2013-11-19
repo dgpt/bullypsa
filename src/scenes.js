c380d7cbe2643fdba06bcea3ca06f94ad2b2ee39
dogpantz <dogpantz@gmail.com>
2013-11-18 20:43:11 -0700

    Fixed weird issue with NPCs
    Removed _.bindAll, replaced with _.bind

--- src/scenes.js
+++ src/scenes.js
@@ -82,8 +82,8 @@ Crafty.scene('Street', function() {
                         ]);
                         cindy._dflag0 = true;
                     }
-                }});
 
+                }});
             speechResponse(function(e) {
                 player.enabled = false;
                 storyFadeOut(_.partial(showGirlLesson, e));
